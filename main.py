be372e5b666968038cea5a34acbeaf89f0db8e70
ahchooindoorair7 <nick@ahchooindoorair.com>
2025-05-26 11:34:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,4 +28,6 @@ def voice():
     return Response(twiml_response, mimetype="application/xml")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
